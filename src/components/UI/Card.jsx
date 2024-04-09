7fff6fcd0b72d91c16c1aa189348a7aa7cdead77
Dmytro <deekorde@gmail.com>
2024-04-09 16:45:34 +0300

    basic components added

---
+++ src/components/UI/Card.jsx
@@ -0,0 +1,15 @@
+import classes from "./Card.module.css";
+
+const Card = ({className, children}) => {
+    return (
+        <section
+            className={`${classes.card} ${
+                className ? className : ""
+            }`}
+        >
+            {children}
+        </section>
+    );
+};
+
+export default Card;
